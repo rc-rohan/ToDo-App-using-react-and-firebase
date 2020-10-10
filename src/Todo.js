06b5a311606dbe4b91ff3960d2ce664be114e7d7
rc-rohan <rohan6hawat@gmail.com>
2020-10-10 22:31:57 +0530

    Added Update Functionality with modal

--- src/Todo.js
+++ src/Todo.js
@@ -1,23 +1,71 @@
-import { Button, List, ListItem, ListItemText } from "@material-ui/core";
-import React from "react";
+import { Modal, List, ListItem, ListItemText, Button } from "@material-ui/core";
+import DeleteForeverIcon from "@material-ui/icons/DeleteForever";
+import React, { useState } from "react";
 import "./Todo.css";
 import db from "./firebase";
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles((theme) => ({
+    paper: {
+        position: "absolute",
+        width: 400,
+        backgroundColor: theme.palette.background.paper,
+        border: "2px solid #333",
+        boxShodow: theme.shadows[5],
+        padding: theme.spacing(2, 4, 3),
+    },
+}));
 
 function Todo({ task }) {
+    const classes = useStyles();
+    const [open, setOpen] = useState(false);
+    const [input, setInput] = useState("");
+
+    const handleOpen = () => {
+        setOpen(true);
+    };
+
+    const updateTodo = () => {
+        // Update the todo here
+        db.collection('todos').doc(task.id).set({
+            todo:input
+        },{merge:true})
+        // merge:true prevents from overiding what things are already there in the colection list
+        setOpen(false);
+    };
+
     return (
-        <List className="todo__list">
-            <ListItem>
-                <ListItemText
-                    primary={task.todo}
-                    secondary="Todo"
-                ></ListItemText>
-            </ListItem>
-            <Button
-                onClick={(e) => db.collection("todos").doc(task.id).delete()}
-            >
-                ❌ Delete Task
-            </Button>
-        </List>
+        <>
+            {/* Error happening here */}
+            {/* Creating the Modal */}
+            <Modal open={open} onClose={(e) => setOpen(false)}>
+                <div className={classes.paper}>
+                    <h1>Open</h1>
+                    <input
+                        type="text"
+                        value={input}
+                        placeholder={task.todo}
+                        onChange={(e) => setInput(e.target.value)}
+                    />
+                    <Button onClick={updateTodo}>Update Todo</Button>
+                </div>
+            </Modal>
+            <List className="todo__list">
+                <ListItem>
+                    <ListItemText
+                        primary={task.todo}
+                        secondary="Todo"
+                    ></ListItemText>
+                </ListItem>
+                {/* Button for the Modal */}
+                <button onClick={(e) => setOpen(true)}>Edit</button>
+                <DeleteForeverIcon
+                    onClick={(e) =>
+                        db.collection("todos").doc(task.id).delete()
+                    }
+                />
+            </List>
+        </>
     );
 }
 
