472e596f83a4058a7bb1dbb63ce21c37689031a6
rc-rohan <rohan6hawat@gmail.com>
2020-10-10 15:07:14 +0530

    Added the basic desing of the Todo App

--- src/App.js
+++ src/App.js
@@ -1,26 +1,47 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import { Button, FormControl, InputLabel ,Input} from "@material-ui/core";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [todos, setTodos] = useState([]);
+    const [input, setInput] = useState("");
+
+    const addTodo = (e) => {
+        e.preventDefault();
+        setTodos([...todos, input]);
+        setInput("");
+    };
+
+    return (
+        <div className="App">
+            <h1>Hello world </h1>
+            <form>
+
+                {/* Basically here we are getting the input from the input variable value and not directly */}
+                <FormControl>
+                    <InputLabel>✅Add Your ToDo</InputLabel>
+                    <Input
+                        value={input}
+                        onChange={(e) => setInput(e.target.value)}
+                    />
+                </FormControl>
+                <Button
+                    disabled={!input}
+                    type="submit"
+                    onClick={addTodo}
+                    variant="contained"
+                    color="primary"
+                >
+                    Add Todo
+                </Button>
+            </form>
+            <ul>
+                {todos.map((todo) => (
+                    <li>{todo}</li>
+                ))}
+            </ul>
+        </div>
+    );
 }
 
 export default App;
